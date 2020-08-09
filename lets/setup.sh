6b2c1077b84bc8117ed4167b8d6b7c8a2f19894c
Wei Tung Chen <weitungchen1998@gmail.com>
2020-08-09 14:46:39 -0700

    handle edge case for mac

--- lets/setup.sh
+++ lets/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo ln -f src/lets /usr/bin/
+sudo mkdir -p /etc/bash_completion.d/
+sudo ln -f src/lets /usr/local/bin/
 sudo ln -f src/lets-completion /etc/bash_completion.d/
 
 echo -e "\nsource /etc/bash_completion.d/lets-completion"
